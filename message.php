1f725b3b741dc34cc2f6e571b07e625f42d57723
AmyIto <ito.emi.web@gmail.com>
2020-03-28 05:22:01 +0900

    表示するメッセージを6つに変更

--- message.php
+++ message.php
@@ -1,16 +1,18 @@
 <?php
-$n = mt_rand(1, 5);
+$n = mt_rand(1, 6);
 
 if ($n === 1) {
-		$message = 'GitHubの勉強中です';
+	$message = 'GitHubの勉強中です';
 } elseif ($n === 2) {
-		$message = 'PHPの勉強中です';
+	$message = 'PHPの勉強中です';
 } elseif ($n === 3) {
-		$message = '次はCakePHPです';
+	$message = '次はCakePHPです';
 } elseif ($n === 4) {
-		$message = 'Laravelも勉強します';
+	$message = 'Laravelも勉強します';
+} elseif ($n === 5) {
+	$message = 'プロサーは面白いです';
 } else {
-		$message = 'プロサーは面白いです';
+	$message = '寿司打を練習します';
 }
 
 ?>
