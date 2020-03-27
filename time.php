2a54f10cf87adb0fbb73f0bbc26997a8af796245
AmyIto <ito.emi.web@gmail.com>
2020-03-28 01:14:10 +0900

    シンガポールの現在時刻を追記

--- time.php
+++ time.php
@@ -3,6 +3,10 @@ function tokyoTime() {
 	date_default_timezone_set('Asia/Tokyo');
 	echo date('Y/m/d H:i');
 }
+function singaporeTime() {
+	date_default_timezone_set('Asia/Singapore');
+	echo date('Y/m/d H:i');
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -11,7 +15,10 @@ function tokyoTime() {
 <title>課題20</title>
 </head>
 <body>
-<h1>日本の現在日時</h1>
+<h1>日本とシンガポールの現在日時</h1>
+<p>日本の現在日時</p>
 <p><?php tokyoTime(); ?></p>
+<p>シンガポールの現在日時</p>
+<p><?php singaporeTime(); ?></p>
 </body>
 </html>
