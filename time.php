1a2c14ed68afd670ff3f0c051a0d4b0b3dc4db6b
AmyIto <ito.emi.web@gmail.com>
2020-03-28 04:52:33 +0900

    initial commit

--- time.php
+++ time.php
@@ -3,28 +3,16 @@ function tokyoTime() {
 	date_default_timezone_set('Asia/Tokyo');
 	echo date('Y/m/d H:i');
 }
-function singaporeTime() {
-	date_default_timezone_set('Asia/Singapore');
-	echo date('Y/m/d H:i');
-}
-function romaTime() {
-	date_default_timezone_set('Europe/Rome');
-	echo date('Y/m/d H:i');
-}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>課題20</title>
+<title>現在時刻表示</title>
 </head>
 <body>
-<h1>東京とシンガポールとローマの現在日時</h1>
-<p>東京の現在日時</p>
+<h1>日本の現在日時</h1>
+<p>日本の現在日時</p>
 <p><?php tokyoTime(); ?></p>
-<p>シンガポールの現在日時</p>
-<p><?php singaporeTime(); ?></p>
-<p>ローマの現在日時</p>
-<p><?php romaTime(); ?></p>
 </body>
 </html>
