2be9309700c3fbed6d89e390bc684bc96787e1f2
AmyIto <ito.emi.web@gmail.com>
2020-03-28 03:25:11 +0900

    イタリアの現在日時を追加

--- time.php
+++ time.php
@@ -7,6 +7,10 @@ function singaporeTime() {
 	date_default_timezone_set('Asia/Singapore');
 	echo date('Y/m/d H:i');
 }
+function romaTime() {
+	date_default_timezone_set('Europe/Rome');
+	echo date('Y/m/d H:i');
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,10 +19,12 @@ function singaporeTime() {
 <title>課題20</title>
 </head>
 <body>
-<h1>日本とシンガポールの現在日時</h1>
+<h1>日本とシンガポールとイタリアの現在日時</h1>
 <p>日本の現在日時</p>
 <p><?php tokyoTime(); ?></p>
 <p>シンガポールの現在日時</p>
 <p><?php singaporeTime(); ?></p>
+<p>イタリアの現在日時</p>
+<p><?php romaTime(); ?></p>
 </body>
 </html>
